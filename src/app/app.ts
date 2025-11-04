208d54c3c727aefd6efb96041cf799d38e876855
bp558 <bp558@njit.edu>
2025-11-04 18:48:25 -0500

    nav work

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,28 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+import { App } from './app';
+import { routes } from './app.routes';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    App,
+    HomeComponent,
+    SignupComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  bootstrap: [App]
 })
-export class App {
-  protected readonly title = signal('notes-frontend');
-}
+export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
+
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
+  constructor(private router: Router) {}
+
+  login() {
+    // Here you would validate credentials
+    // For now, just navigate to dashboard
+    this.router.navigate(['/dashboard']);
+  }
 }
+
