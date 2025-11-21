992d66f690414db4f0e3ca10fb688d78313d75a7
bp558 <bp558@njit.edu>
2025-11-21 16:25:54 -0500

    main.ts had duplicate imports

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -2,16 +2,13 @@ import { inject } from '@angular/core';
 import { HttpInterceptorFn, HttpRequest, HttpHandlerFn } from '@angular/common/http';
 import { AuthService } from './services/auth.service';
 
-export const AuthInterceptor: HttpInterceptorFn = (req, HttpHandlerFn) => {
+export const AuthInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn) => {
   const auth = inject(AuthService);
   const token = auth.getToken();
-
-  console.log("INTERCEPTOR FIRED:", req.url);
-  console.log("TOKEN:", token);
 
   const authReq = token
     ? req.clone({ setHeaders: { Authorization: `Bearer ${token}` } })
     : req;
 
-  return HttpHandlerFn(authReq);
+  return next(authReq);
 };
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
-import { routes } from './app/app.routes';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { AuthInterceptor } from './app/auth.interceptor';
+import { appConfig } from './app/app.config';
 
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi()) // <-- changed
-  ]
-})
-.catch(err => console.error(err));
-
+bootstrapApplication(AppComponent, appConfig)
+  .catch(err => console.error(err));
