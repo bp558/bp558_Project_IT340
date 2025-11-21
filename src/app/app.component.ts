2dac1106775af5cd61ff454fba853c0a452bf56e
bp558 <bp558@njit.edu>
2025-11-21 10:15:26 -0500

    debug dasboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NoteEditorComponent } from './note-editor/note-editor.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    DashboardComponent,
+    NoteEditorComponent
+  ],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
 export class AppComponent {}
-
