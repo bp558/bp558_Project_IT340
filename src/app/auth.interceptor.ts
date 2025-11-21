6b08367ef8c3f1d73774d7e56ab22dafde78880d
bp558 <bp558@njit.edu>
2025-11-21 16:17:55 -0500

    notes logic

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -2,13 +2,16 @@ import { inject } from '@angular/core';
 import { HttpInterceptorFn, HttpRequest, HttpHandlerFn } from '@angular/common/http';
 import { AuthService } from './services/auth.service';
 
-export const AuthInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn) => {
+export const AuthInterceptor: HttpInterceptorFn = (req, HttpHandlerFn) => {
   const auth = inject(AuthService);
   const token = auth.getToken();
+
+  console.log("INTERCEPTOR FIRED:", req.url);
+  console.log("TOKEN:", token);
 
   const authReq = token
     ? req.clone({ setHeaders: { Authorization: `Bearer ${token}` } })
     : req;
 
-  return next(authReq);
+  return HttpHandlerFn(authReq);
 };
