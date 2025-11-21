dae51dd0ab5bb218a7f05154d320259a79fece06
bp558 <bp558@njit.edu>
2025-11-21 15:04:46 -0500

    notes logic

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,18 +1,15 @@
-import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable, inject } from '@angular/core';
+import { HttpInterceptorFn, HttpRequest, HttpHandlerFn } from '@angular/common/http';
 import { AuthService } from './services/auth.service';
 
-@Injectable()
-export class AuthInterceptor implements HttpInterceptor {
-  constructor(private auth: AuthService) {}
+@Injectable({ providedIn: 'root' })
+export const AuthInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn) => {
+  const auth = inject(AuthService);
+  const token = auth.getToken();
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = this.auth.getToken();
-    if (token) {
-      const cloned = req.clone({ setHeaders: { Authorization: `Bearer ${token}` } });
-      return next.handle(cloned);
-    }
-    return next.handle(req);
-  }
-}
+  const authReq = token
+    ? req.clone({ setHeaders: { Authorization: `Bearer ${token}` } })
+    : req;
+
+  return next(authReq);
+};
